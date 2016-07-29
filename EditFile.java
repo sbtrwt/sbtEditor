becacbc12626a42f29e39121105601c96b15ba2e
sbtrwt <sbtrwt.se@gmail.com>
2016-07-29 12:32:58 +0530

    file operation
    Added file operation in editor. this is just a basic file to add.

---
+++ EditFile.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileInputStream;
+
+public class EditFile {
+  static String strLastContent;
+  
+    public static void Save(String strFileName, String strContent) throws IOException{
+          FileWriter out = null;
+          try{
+            File file = new File(strFileName);
+            if (file.exists() && file.isFile()){
+            load(strFileName);
+            }
+      if(strLastContent != null && !strLastContent.isEmpty())
+      	  {
+      	  }
+         out = new FileWriter(strFileName,true);
+         
+		char[] charArray = strContent.toCharArray();
+         int indx =0 , Length = charArray.length;
+         while (indx < Length) {
+            out.write(charArray[indx++]);
+         }
+      
+        }finally {
+         if (out != null) {
+            out.close();
+         }
+      }
+    }
+    
+     public static String load( String inputFile) throws IOException {
+		FileReader inputReader = null;
+		try{
+			inputReader = new FileReader(inputFile);
+			int c;
+			while ((c = inputReader.read()) != -1) {
+			strLastContent += String.valueOf(c);
+			}
+		}		finally{
+			if(inputReader != null){
+					inputReader.close();
+			}
+		}
+		return strLastContent;
+	}
+ /*  public static void copyWith1Byte(String input, String output) throws IOException
+   {
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+         in = new FileInputStream(input);
+         out = new FileOutputStream(output);
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      	}
+   }*/
+ public static void copyWith2Byte(String input, String output) throws IOException
+   {
+      FileReader in = null;
+      FileWriter out = null;
+
+      try {
+         in = new FileReader(input);
+         out = new FileWriter(output);
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+   
+}
