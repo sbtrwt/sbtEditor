93631617bc6da63b9785fee9b5b6760d7b262250
sbtrwt <sbtrwt.se@gmail.com>
2016-07-29 14:10:35 +0530

    file save
    file save operation, actionlistener + support with ctrl+s save

---
+++ customKeyListener.java
@@ -0,0 +1,21 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class customKeyListener implements KeyListener {
+	Texteditor teKeyEvent;
+	public customKeyListener(Texteditor teFound){
+		teKeyEvent = teFound;
+	}
+   @Override
+	public void keyPressed(KeyEvent e){
+	    if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
+                   teKeyEvent.saveFile("sbt.txt");
+                }
+	}
+   @Override
+	public void keyReleased(KeyEvent e) {System.out.println("key Pressed....");}
+   @Override
+	public void keyTyped(KeyEvent e){System.out.println("key Pressed....");}
+
+}
